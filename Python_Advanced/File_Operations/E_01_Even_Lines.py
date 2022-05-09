bb387e58d14e6e9cfb383591b68b044ee4994e25
misteryco <jordan.v.dimov@gmail.com>
2022-05-09 14:21:17 +0300

    pak ima neshto

--- Python_Advanced/File_Operations/E_01_Even_Lines.py
+++ Python_Advanced/File_Operations/E_01_Even_Lines.py
@@ -4,10 +4,12 @@ import re
 # Function that creates text.txt with few standard content for program test, if you want different input file content,
 # don't yse the function below.
 def clean_test_file():
-    txt_data = ["-I was quick to judge him, but it wasn't his fault.", "-Is this some kind of joke?! Is it?", "-Quick, hide here. It is safer."]
+    txt_data = ["-I was quick to judge him, but it wasn't his fault.",
+                "-Is this some kind of joke?! Is it?",
+                "-Quick, hide here. It is safer."]
     start_file = open("text.txt", "w")
-    for line in txt_data:
-        start_file.write(f"{line} \n")
+    for row in txt_data:
+        start_file.write(f"{row} \n")
     start_file.close()
 
 
