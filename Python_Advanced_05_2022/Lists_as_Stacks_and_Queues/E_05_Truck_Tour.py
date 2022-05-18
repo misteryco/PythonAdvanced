5dbc9b673801e943b989e92913bf1df53f449376
misteryco <jordan.v.dimov@gmail.com>
2022-05-18 18:24:53 +0300

    pak ima neshto

--- Python_Advanced_05_2022/Lists_as_Stacks_and_Queues/E_05_Truck_Tour.py
+++ Python_Advanced_05_2022/Lists_as_Stacks_and_Queues/E_05_Truck_Tour.py
@@ -9,20 +9,17 @@ for pump in range(pumps):
     counter += 1
 
 available_fuel = 0
-flag = False
 for pump_ in range(len(pumps_list)):
-    flag = False
+    fail = False
     available_fuel = 0
-    for idx in range(len(pumps_list)):
-        current_pump_fuel = pumps_list[idx][0]
-        next_pump_distance = pumps_list[idx][1]
+    for current_pump_fuel, next_pump_distance, pump_index in pumps_list:
         available_fuel += current_pump_fuel
         if available_fuel >= next_pump_distance:
             available_fuel -= next_pump_distance
         else:
-            flag = True
+            fail = True
             break
-    if flag:
+    if fail:
         pumps_list.append(pumps_list.popleft())
     else:
         break
--- Python_Advanced_05_2022/Lists_as_Stacks_and_Queues/E_06_Balanced_Parentheses.py
+++ Python_Advanced_05_2022/Lists_as_Stacks_and_Queues/E_06_Balanced_Parentheses.py
@@ -1,15 +1,14 @@
+
 string = list(input())
 # print(string)
 stack = []
 open_parentheses = []
-if len(string) %2 == 0:
+if len(string) % 2 == 0:
     for char in string:
-        if char == "{":
-            open_parentheses.append(("{", "}"))
-        elif char == "(":
-            open_parentheses.append(("(", ")"))
-        elif char == "[":
-            open_parentheses.append(("[", "]"))
+        if char in "{([":
+            open_parentheses.append(char)
+
+#  Do it with pairs dict !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         elif char == "}" and open_parentheses[-1][0] == "{":
             open_parentheses.pop()
